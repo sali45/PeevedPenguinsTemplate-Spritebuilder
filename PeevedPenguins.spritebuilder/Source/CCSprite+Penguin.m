95d19e20bf26d0478aeec1485a47368eb09b9d00
sali45 <sali45@gatech.edu>
2015-04-04 10:19:20 -0400

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Source/CCSprite+Penguin.m
+++ PeevedPenguins.spritebuilder/Source/CCSprite+Penguin.m
@@ -9,13 +9,5 @@
 #import "CCSprite+Penguin.h"
 
 @implementation CCSprite (Penguin)
-- (id)init {
-    self = [super init];
-    
-    if (self) {
-        CCLOG(@"Penguin created");
-    }
-    
-    return self;
-}
+
 @end
