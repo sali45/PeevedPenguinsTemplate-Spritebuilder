2db68a1161e05dde8f0252545cdd37bb73d01584
sali45 <sali45@gatech.edu>
2015-04-03 22:20:59 -0400

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Source/AppDelegate.m
+++ PeevedPenguins.spritebuilder/Source/AppDelegate.m
@@ -54,6 +54,9 @@
     
     [self setupCocos2dWithOptions:cocos2dSetup];
     
+    [CCBReader load:@"Penguin"];
+    [CCBReader load:@"Seal"];
+    
     return YES;
 }
 
@@ -62,15 +65,4 @@
     return [CCBReader loadAsScene:@"MainScene"];
 }
 
-- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
-{
-    [...]
-    
-    [self setupCocos2dWithOptions:cocos2dSetup];
-    
-    [CCBReader load:@"Penguin"];
-    [CCBReader load:@"Seal"];
-    
-    return YES;
-}
 @end
