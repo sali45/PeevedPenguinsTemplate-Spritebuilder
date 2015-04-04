546017d837baab28b899c2dffd61086281ae4c2d
sali45 <sali45@gatech.edu>
2015-04-04 10:20:25 -0400

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Source/libs/cocos2d-iphone/cocos2d/cocos2d.h
+++ PeevedPenguins.spritebuilder/Source/libs/cocos2d-iphone/cocos2d/cocos2d.h
@@ -127,7 +127,6 @@
 #import "CCPhysicsShape.h"
 
 // Sound
-#import "OALSimpleAudio.h"
 
 // Retiring
 #import "CCAnimation.h" // put this back for v3.4 because it's still in use, and would otherwise be unavailable to Swift
